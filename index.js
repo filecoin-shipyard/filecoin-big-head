ff3afabe851c976d36596949af2a74220b44e4bb
Jim Pick <jim@backblend.com>
2019-04-06 14:07:44 -0700

    Rename, add bin

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+#!/usr/bin/env node
+
 require = require("esm")(module/*, options*/)
 const importJsx = require('import-jsx')
 process.env.FORCE_COLOR = '1'
-importJsx('./counter')
+importJsx('./main')
