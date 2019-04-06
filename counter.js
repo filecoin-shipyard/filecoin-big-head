9fac6d37dbaf9dd0813e0607b96dedb0dc5513bd
Jim Pick <jim@backblend.com>
2019-04-05 18:59:39 -0700

    Use react hooks

--- counter.js
+++ counter.js
@@ -1,50 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { render, Box, Color } from 'ink'
 import InkBox from 'ink-box'
-import Gradient from 'ink-gradient'
 import BigText from 'ink-big-text'
 
-class Counter extends React.Component {
-	constructor() {
-		super();
+const Counter = () => {
+  const [count, setCount] = useState(0)
 
-		this.state = {
-			i: 0
-		};
-	}
+  useEffect(() => {
+    let effectCount = count
+    const intervalId = setInterval(() => { setCount(effectCount++) }, 1000)
+    return () => clearInterval(intervalId)
+  }, true)
 
-	render() {
-    const { columns, rows } = process.stdout
-		return (
-      <Box flexDirection="column" width={columns} height={rows - 1}>
-        <Box>
-          Label:
-        </Box>
-        <Box textWrap="truncate">
-          <Color green>
-            {this.state.i} tests passed
-          </Color>
-          {'x'.repeat(200)}
-        </Box>
-        <Box flexGrow={1} textWrap="truncate">
-          Test...............................................................
-          <BigText text={String(this.state.i)} font="huge" />
-        </Box>
+  const { columns, rows } = process.stdout
+
+  return (
+    <Box flexDirection="column" width={columns} height={rows - 1}>
+      <Box>
+        Label:
       </Box>
-		)
-	}
-
-	componentDidMount() {
-		this.timer = setInterval(() => {
-			this.setState({
-				i: this.state.i + 1
-			});
-		}, 500);
-	}
-
-	componentWillUnmount() {
-		clearInterval(this.timer);
-	}
+      <Box textWrap="truncate">
+        <Color green>
+          {count} tests passed
+        </Color>
+        {'x'.repeat(200)}
+      </Box>
+      <Box flexGrow={1} textWrap="truncate">
+        Test...............................................................
+        <BigText text={String(count)} font="huge" />
+      </Box>
+    </Box>
+  )
 }
 
 render(<Counter/>);
