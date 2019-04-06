ce6781b0cd0ed753889b4285794fe09ddc04b977
Jim Pick <jim@backblend.com>
2019-04-05 18:38:43 -0700

    Rename, try out ink-big-text

--- counter.js
+++ counter.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render, Box, Color } from 'ink'
 import InkBox from 'ink-box'
+import Gradient from 'ink-gradient'
+import BigText from 'ink-big-text'
 
 class Counter extends React.Component {
 	constructor() {
@@ -12,24 +14,21 @@ class Counter extends React.Component {
 	}
 
 	render() {
-    /*
-        <InkBox borderStyle="round" borderColor="cyan" float="center" padding={1}>
-          <Color green>
-            {this.state.i} tests passed
-          </Color>
-        </InkBox>
-        */
     const { columns, rows } = process.stdout
 		return (
       <Box flexDirection="column" width={columns} height={rows - 1}>
         <Box>
           Label:
         </Box>
-        <Box flexGrow={1} textWrap="truncate">
+        <Box textWrap="truncate">
           <Color green>
             {this.state.i} tests passed
           </Color>
           {'x'.repeat(200)}
+        </Box>
+        <Box flexGrow={1} textWrap="truncate">
+          Test...............................................................
+          <BigText text={String(this.state.i)} font="huge" />
         </Box>
       </Box>
 		)
@@ -40,7 +39,7 @@ class Counter extends React.Component {
 			this.setState({
 				i: this.state.i + 1
 			});
-		}, 100);
+		}, 500);
 	}
 
 	componentWillUnmount() {
